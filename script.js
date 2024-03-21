ec6057adce5a9e2c63d523ee892a5331c3d3bb6a
alexissma <alexisma03@gmail.com>
2024-03-21 09:00:51 -0300

    teste

---
+++ script.js
@@ -0,0 +1,26 @@
+let var1 = "30";
+let var2 = 5;
+
+let resultado1 = var1 - var2;
+let resultado2 = var1 + var2;
+
+console.log(var1 * var2);
+console.log(var1 / var2);
+
+console.log(resultado1);
+console.log(typeof(resultado1))
+
+console.log(resultado2);
+console.log(typeof(resultado2))
+
+
+/*
+let variavel1 = 0;
+variavel1 = "sarrador profissional, berti lucas"
+
+const PI = 3.14;
+
+console.log(typeof(variavel1));
+
+PI = 3.15;
+*/
